656b3dded89c8f2c4c6b3c13998b05e806a4946e
Joe Oguri <joguri123@gmail.com>
2016-06-19 04:52:12 +0000

    fixes

--- lab9/Servo.py
+++ lab9/Servo.py
@@ -4,12 +4,12 @@ class Servo:
 
     def __init__(self, pin):
         self.pin = pin
-        GPIO.setup(self.pin, GPIO.OUTPUT)
+        GPIO.setup(self.pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pin, 100)
-        self.start(10);
+        self.pwm.start(10);
 
     def moveTo(self, degrees):
-        self.pwm.changeDutyCycle(degrees)
+        self.pwm.ChangeDutyCycle(degrees)
 
     def stop(self):
         self.pwm.stop()
