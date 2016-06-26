c3043879b5ea9d50ccd45eaee718e56d8e4fb98d
Joe Oguri <joguri123@gmail.com>
2016-06-26 04:54:32 +0000

    pigs

--- lab9/webServer.py
+++ lab9/webServer.py
@@ -1,13 +1,19 @@
 import flask
 import json
-import Servo
 import RPi.GPIO as GPIO
+
+import Servo
+import Motor
 
 GPIO.setmode(GPIO.BCM)
 
 app = flask.Flask(__name__)
-panServo = Servo.Servo(14)
-tiltServo = Servo.Servo(15)
+panServo = Servo.Servo(24)
+tiltServo = Servo.Servo(25)
+motor = Motor.Motor(14,15,18)
+
+motor.forward()
+motor.setSpeed(0)
 
 @app.route("/home/<path>")
 def index(path):
@@ -29,18 +35,22 @@ def setTilt(tilt):
 
 @app.route('/motor/speed/<speed>')
 def stopNow(speed):
+    motor.setSpeed(int(speed))
     return json.dumps({'cmd':'motor', 'value':speed})
 
 @app.route('/motor/forward')
 def motorForward():
+    motor.forward()
     return json.dumps({'cmd':'motorForward'});
 
 @app.route('/motor/reverse')
 def motorReverse():
+    motor.reverse()
     return json.dumps({'cmd':'motorReverse'});
 
 @app.route('/motor/stop')
 def motorStop():
+    motor.stop()
     return json.dumps({'cmd':'motorStop'});
 
 app.run(host="10.0.0.30", debug=True)
